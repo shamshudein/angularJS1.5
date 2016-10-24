d579de448b5bb6554439acb478c593357dd0b309
shamshudein <ShamshudeinSyed.Kareem@cognizant.com>
2016-10-24 15:23:12 +0530

    initial

---
+++ js/hello-world-component.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+
+    var module = angular.module("helloApp");
+
+    module.component("helloWorld",{
+        //template: "Hello from Component"
+        templateUrl: "templates/hello-world-template.html"
+    });
+
+}());
